31d64dd27d668533d5106631e0ea8a43c0732ec3
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-09-05 20:20:32 +0600

    intro to JSON

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log('Explore API');
+
+const person = {
+    name: 'selim',
+    fruit: 'dalim',
+    dish: 'halim',
+    friends: ['alim', 'kolim', 'lamim'],
+    isRich: false,
+    money: 34000,
+};
+console.log(person, typeof person);
+
+// JSON --> JS object with Notification
+// JSON.stringify --> JSON
+// JSON.parse --> Object
+
+const personJSON = JSON.stringify(person)
+console.log(personJSON, typeof personJSON)
+
+
+const parseJSON = JSON.parse(personJSON);
+console.log(parseJSON, typeof parseJSON)
