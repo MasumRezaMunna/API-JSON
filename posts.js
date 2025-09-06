84b89ac3dfdaca75dc8fd32b6ff9459e5b8e611e
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-09-06 18:05:17 +0600

    load and display post title list in the UI

---
+++ posts.js
@@ -0,0 +1,29 @@
+const loarPost = () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        displayPosts(data);
+    });
+};
+
+const displayPosts = (posts) => {
+
+    // 1. get the container
+    const postContainer = document.getElementById ("post-container");
+    console.log(postContainer)
+
+
+    posts.forEach((post) => {
+        console.log(post.title);
+
+        // 2. create HTML element
+        const li = document.createElement("li");
+        li.innerText = post.title;
+        console.log(li)
+
+        // 3. add li into container
+        postContainer.appendChild(li)
+    });
+};
